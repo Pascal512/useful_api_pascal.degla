6e5c33746612d343d425425c512af0d0e9adb00e
Pascal512 <pascal.degla@epitech.eu>
2025-10-16 12:56:48 +0100

    feat: Setup user login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validated = $request->validate([
+        $request->validate([
             'email' => 'required|unique:users,email|max:255',
             'password' => 'required|min:8',
             'name' => 'required|max:255',
@@ -31,6 +32,24 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required|exists:users,email|max:255',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => "Unauthorized, wrong credentials",
+            ], 401);
+        }
+
+        $token = $user->createToken($user->name);
+
+        return response()->json([
+            'token' => $token->plainTextToken,
+            'user_id' => $user->id,
+        ], 200);
     }
 }
