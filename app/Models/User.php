42c6541ec26490f75894da8ddc8bbb0c3806adb4
Pascal512 <pascal.degla@epitech.eu>
2025-10-16 11:40:21 +0100

    feat: Defined Module model, migration, seeder and relation with User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * The modules that belong to the user.
+     */
+    public function modules(): BelongsToMany
+    {
+        return $this->belongsToMany(Module::class, 'user_modules')
+            ->withPivot('active')
+            ->withPivotValue('active', false);
     }
 }
